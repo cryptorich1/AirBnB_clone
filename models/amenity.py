822138b5a630512b110273b136e834d6bc04aba9
Hendrix Ebrudu <hendrixobuks@gmail.com>
2023-02-13 02:06:13 +0100

    edit

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ Define the Amenity class
 from models.base_model import BaseModel
 
 
-clase Amenity(BaseModel):
+class Amenity(BaseModel):
     """Represent an amenity.
     Attribute:
     name (str): The name of the Amenity
